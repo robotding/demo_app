1de3ccbfe5e6bc4fa24edebbe37466f78bbf76d6
robotding <robotding@gmail.com>
2014-06-04 08:50:08 -0700

    Initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '5c9ee525096171606537e2a740af30377bdb038f17f516c9e77e3171aa413219de22d0d4efcebc61804ce2e91c0e500c3996d7fb61546ebaa70fd975bc3e6153'
